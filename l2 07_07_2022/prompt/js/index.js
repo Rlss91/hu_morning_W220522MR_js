5074cd3eec32548cb109d647205b56b6de8aafd8
Rlss91 <rlss91@gmail.com>
2022-07-07 14:25:03 +0300

    prompt

---
+++ l2 07_07_2022/prompt/js/index.js
@@ -0,0 +1,14 @@
+var age = prompt("please enter your age");
+console.log(age);
+
+/*
+    prompt can return 
+    1) string with the user input
+    2) string empty ""
+    3) if the user press on cancel then prompt will return null
+*/
+
+//if the return value is not null and not empty string
+//this mean that the user write something and press on ok btn
+if (age != null && age != "") {
+}
