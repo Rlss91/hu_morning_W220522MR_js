38e1d1b36aa913d36abb3d248d39aeca23ac52b3
Rlss91 <rlss91@gmail.com>
2022-07-12 14:37:36 +0300

    loops and html

--- l4 12_07_2022/loops/js/index.js
+++ l4 12_07_2022/loops/js/index.js
@@ -1,9 +1,13 @@
 let animalArr = ["dog", "puma", "ostrige", "wheil", "arbok", "pikachu"];
 
 let colHtml = "";
+//the for loop will go throw every cell in animalArr
 for (let i = 0; i < animalArr.length; i++) {
+  // each animal we will create bs col and alert
+  // we will add to each alert unique id so we can access the elm when needed
+  // add to each alert text with the animal name from array
   colHtml += `<div class="col">
-                    <div class="alert alert-primary" role="alert">${animalArr[i]}</div>
+                    <div class="alert alert-primary" role="alert" id="animal_${i}">${animalArr[i]}</div>
             </div>`;
   //   colHtml =
   //     colHtml +
@@ -14,3 +18,17 @@ for (let i = 0; i < animalArr.length; i++) {
 // console.log(colHtml);
 
 document.getElementById("animalrow").innerHTML = colHtml;
+
+function handleAnimalClick() {
+  let animalInput = document.getElementById("animalInput").value;
+  //the for loop will go throw every cell in animalArr
+  for (let i = 0; i < animalArr.length; i++) {
+    //if the animal that the user was typed equal to cell in the array
+    if (animalInput === animalArr[i]) {
+      //then go to the selected element and change the background color to green
+      document.getElementById(`animal_${i}`).style.backgroundColor = "green";
+      //stop the loop because you already found the animal
+      break;
+    }
+  }
+}
